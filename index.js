2bb7c2146f555e4d2425b1366a027827548310cd
Ilya Vlasov <ilyavlasov93@gmail.com>
2021-03-23 15:19:15 -0400

    draw added

--- index.js
+++ index.js
@@ -1,95 +1,75 @@
-let turn = 1; 
+let turn = 0; 
 let chare = "X";
 let play = [
-[0,1,2],
-[3,4,5],
-[6,7,8],
-[0,3,6],
-[1,4,7],
-[2,5,8],
-[0,4,8],
-[2,4,6]
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
 ];
-let position = ["","","","","","","","",""];
 
+let position = [];
 
-
-document.querySelector('.box').onclick = function() {
-  // let raw = document.querySelector('.raw').value;
-  // let colume = document.querySelector('.colume').value;
-  // if (play[raw][colume] != "X" && play[raw][colume] != "O"){
+function check() {
+  let result = [];
+  let rows = document.getElementsByTagName('table')[0].rows;
+  let cells, t;
   
-  // let rowes = document.querySelectorAll('tr');
-  // let celes = rowes[raw].querySelectorAll('td');
-  // let cell = celes[colume];
-  let element = event.target
-  element.innerHTML = chare
-  let node = element.parentNode
-  let i = ([...node.children].indexOf(element));
-  position[i] = chare;
+  for (let a = 0, alength = rows.length; a < alength; a++ ){
+    cells = rows[a].cells;
+    t = [];
+  
 
-  if(turn%2 != 1){
-    chare = "X";
-    document.querySelector('span').innerHTML = "X";
-    // cell.style.backgroundColor = "red";
-  } else {
-    chare = 'O'; 
-    document.querySelector('span').innerHTML = "O";
-    // cell.style.backgroundColor = "green";
+    for (let b = 0, blength = cells.length; b < blength; b++){
+      t.push(cells[b].textContent);
+    };
+    result.push(t);
   }
 
-
-  
-  // play[raw][colume] = chare;
-  // cell.innerHTML = chare;
-
-  turn++;
-
-  // if (winer() == 'X'){
-  //   alert("Победили Х")
-  // }
-
+  position = [].concat(...result);
   
 
 
-  console.log(play);
+  console.log(result);
   console.log(position);
 }
 
 
-// function winer() {
-//   if(play[0][0] == 'X' && play[0][1] == 'X' && play[0][2] == 'X')
-//   {return "X"}
-//   if(play[1][0] == 'X' && play[1][1] == 'X' && play [1][2] == 'X')
-//   {return "X"}
-//   if(play[2][0] == 'X' && play[2][1] == 'X' && play [2][2] == 'X')
-//   {return "X"}
-//   if(play[0][0] == 'X' && play[1][0] == 'X' && play[2][0] == 'X') 
-//   {return "X"}
-//   if(play[0][1] == 'X' && play[1][1] == 'X' && play [2][1] == 'X')
-//   {return "X"}
-//   if(play[0][2] == 'X' && play[1][2] == 'X' && play [2][2] == 'X')
-//   {return "X"}
-//   if(play[0][1] == 'X' && play[1][1] == 'X' && play [2][1] == 'X')
-//   {return "X"}
-//   if(play[0][2] == 'X'&& play[1][2] == 'X' && play [2][2] == 'X') 
-//   {return "X"}
-//   if(play[0][0] == 'X' && play[1][1] == 'X' && play [2][2] == 'X')
-//   {return "X"}
-//   if(play[0][2] == 'X'&& play[1][1] == 'X' && play [2][0] == 'X')
-//   {return "X"}
-//   // else if (
-//   // (play[0][0] == 'O' && play[0][1] == 'O' && play[0][2] == 'O'||
-//   // play[1][0] == 'O' && play[1][1] == 'O' && play [1][2] == 'O'||
-//   // play[2][0] == 'O' && play[2][1] == 'O' && play [2][2] == 'O'||
-//   // play[0][0] == 'O' && play[1][0] == 'O' && play[2][0] == 'O' ||
-//   // play[0][1] == 'O' && play[1][1] == 'O' && play [2][1] == 'O'||
-//   // play[0][2] == 'O' && play[1][2] == 'O' && play [2][2] == 'O'||
-//   // play[0][1] == 'O' && play[1][1] == 'O' && play [2][1] == 'O'||
-//   // play[0][2] == 'O'&& play[1][2] == 'O' && play [2][2] == 'O' ||
-//   // play[0][0] == 'O' && play[1][1] == 'O' && play [2][2] == 'O'||
-//   // play[0][2] == 'O'&& play[1][1] == 'O' && play [2][0] == 'O')
-//   // ){
-//   //   winer = alert ("O победили!")
-//   // };
-// }
+document.querySelector('.box').onclick = function() {
+  let element = event.target
+
+  if(element.innerHTML == ''){
+    element.innerHTML = chare
+    turn++;
+  }
+
+  if(turn%2 != 1){
+    chare = "X";
+    document.querySelector('span').innerHTML = "X";
+  } else {
+    chare = 'O'; 
+    document.querySelector('span').innerHTML = "O";
+  }
+
+  check();
+
+  play.forEach((c) => {
+    if (position[c[0]]===chare && position[c[1]]===chare && position[c[2]]===chare){
+          alert("ЗАЕБИСЬ!")
+          window.location.reload();
+        } else if (position.indexOf("") == -1) {
+          alert();
+          window.location.reload();
+        }
+  }); 
+
+
+}
+
+document.querySelector(".submit").onclick = function(){
+  window.location.reload()
+}
+
